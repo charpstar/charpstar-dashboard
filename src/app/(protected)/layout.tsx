2d54cc1c3d2bb1ba4b0f0ed66f889ed982fadae8
Ilyes Khobizi <khobizi.ilyes@gmail.com>
2024-05-01 12:30:14 +0100

    Added layout protection.

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,5 +1,18 @@
-import AppLayout from "@/components/AppLayout";
+import { redirect } from "next/navigation";
 
-export default function ProtectedLayout({ children }: React.PropsWithChildren) {
+import AppLayout from "@/components/AppLayout";
+import { createClient } from "@/utils/supabase/server";
+
+export default async function ProtectedLayout({
+  children,
+}: React.PropsWithChildren) {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) return redirect("/login");
+
   return <AppLayout>{children}</AppLayout>;
 }
