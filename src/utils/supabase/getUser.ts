f4f272ee73ba2aeff98d03eb564793a882c98cb2
Ilyes Khobizi <khobizi.ilyes@gmail.com>
2024-05-09 22:05:22 +0100

    getUserWithMetadata Utility.

---
+++ src/utils/supabase/getUser.ts
@@ -0,0 +1,19 @@
+import type { SupabaseClient } from "@supabase/supabase-js";
+
+export async function getUserWithMetadata(supabase: SupabaseClient) {
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) return null;
+
+  const { data: metadata, error } = await supabase
+    .from("profiles")
+    .select("projectId:projectid, datasetId:datasetid, tableName:tablename")
+    .eq("id", user.id)
+    .single();
+
+  if (error) return null;
+
+  return { ...user, metadata };
+}
