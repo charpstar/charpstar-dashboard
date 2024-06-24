ab0c40734eae892bac4a80967be5fa3a4f3cbdc6
Ilyes Khobizi <khobizi.ilyes@gmail.com>
2024-06-24 13:07:26 +0100

    Added client analytics_274422295

--- src/utils/BigQuery/clientQueries.ts
+++ src/utils/BigQuery/clientQueries.ts
@@ -308,4 +308,167 @@ WHERE total_button_clicks > 0 OR purchases_with_service > 0
 SELECT * FROM final
 WHERE total_button_clicks > 0 OR purchases_with_service > 0
 `,
+
+  analytics_274422295: (eventsBetween: string) => `WITH
+  -- This CTE Grabs all the product names from the items array
+  all_products AS (
+    SELECT DISTINCT
+      TRIM(i.item_name) AS product_name
+    FROM
+      \`fast-lattice-421210.analytics_274422295.events_*\`, UNNEST(items) AS i
+    WHERE ${eventsBetween}
+  ),
+  -- This CTE Grabs all the two click events and their timestamp along with their session id for every user
+  click_events AS (
+    SELECT DISTINCT
+      user_pseudo_id,
+      event_name,
+      event_timestamp AS click_timestamp,
+      (SELECT value.int_value FROM UNNEST(event_params) WHERE key = 'ga_session_id') AS ga_session_id
+    FROM
+      \`fast-lattice-421210.analytics_274422295.events_*\`
+    WHERE
+      event_name IN ('charpstAR_AR_Button_Click', 'charpstAR_3D_Button_Click')
+      AND ${eventsBetween}
+  ),
+  -- This CTE extracts the product name from the event parameter 'page_title'
+  click_events_with_products AS (
+    SELECT DISTINCT
+      event_timestamp AS click_timestamp,
+      user_pseudo_id,
+      event_name,
+      SPLIT(REGEXP_REPLACE((SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'page_title'), 'â€“', '-'), '-')[SAFE_OFFSET(0)] AS page_title_product_name
+    FROM
+      \`fast-lattice-421210.analytics_274422295.events_*\`
+    WHERE
+      event_name IN ('charpstAR_AR_Button_Click', 'charpstAR_3D_Button_Click')
+      AND ${eventsBetween}
+  ),
+  -- This CTE counts the number of times the 'charpstAR_AR_Button_Click' event was triggered
+  ar_clicks AS (
+    SELECT
+      TRIM(page_title_product_name) AS page_title_product_name,
+      COUNT(DISTINCT click_timestamp) AS AR_Button_Clicks
+    FROM
+      click_events_with_products
+    WHERE
+      event_name = 'charpstAR_AR_Button_Click'
+    GROUP BY TRIM(page_title_product_name)
+  ),
+  -- This CTE counts the number of times the 'charpstAR_3D_Button_Click' event was triggered
+  _3d_clicks AS (
+    SELECT
+      TRIM(page_title_product_name) AS page_title_product_name,
+      COUNT(DISTINCT click_timestamp) AS _3D_Button_Clicks
+    FROM
+      click_events_with_products
+    WHERE
+      event_name = 'charpstAR_3D_Button_Click'
+    GROUP BY TRIM(page_title_product_name)
+  ),
+  -- This CTE gets the transaction id once a purchase event happens for a user
+  purchases AS (
+    SELECT DISTINCT
+      user_pseudo_id,
+      event_timestamp,
+      (SELECT value.int_value FROM UNNEST(event_params) WHERE key = 'ga_session_id') AS ga_session_id,
+      (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'transaction_id') AS transaction_id
+    FROM
+      \`fast-lattice-421210.analytics_274422295.events_*\`
+    WHERE
+      event_name = 'purchase'
+      AND ${eventsBetween}
+  ),
+  -- This CTE accounts only those transaction ids that happened after those click events in that session
+  tran_ids_required AS (
+    SELECT DISTINCT p.transaction_id
+    FROM click_events AS c
+    INNER JOIN purchases AS p
+      ON c.ga_session_id = p.ga_session_id
+      AND c.user_pseudo_id = p.user_pseudo_id
+      AND p.event_timestamp > c.click_timestamp
+  ),
+  -- This CTE grabs the product name with their transaction ids to identify purchased products
+  products_purchased_cte AS (
+    SELECT DISTINCT
+      TRIM(i.item_name) AS product_name,
+      (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'transaction_id') AS transaction_id
+    FROM
+      \`fast-lattice-421210.analytics_274422295.events_*\`, UNNEST(items) AS i
+    WHERE
+      event_name = 'purchase'
+      AND ${eventsBetween}
+  ),
+  -- This CTE contains products purchased after the click events along with the cleaned product names
+  products_purchased_after_click_events AS (
+    SELECT
+      product_name,
+      COUNT(DISTINCT transaction_id) AS purchases_with_service
+    FROM
+      products_purchased_cte
+    WHERE
+      transaction_id IN (SELECT DISTINCT transaction_id FROM tran_ids_required)
+    GROUP BY product_name
+  ),
+  -- Total views per product
+  total_views AS (
+    SELECT
+      TRIM(SPLIT(REGEXP_REPLACE(ep.value.string_value, 'â€“', '-'), '-')[SAFE_OFFSET(0)]) AS product_name,
+      COUNT(DISTINCT ev.user_pseudo_id) AS total_views
+    FROM
+      \`fast-lattice-421210.analytics_274422295.events_*\` ev,
+      UNNEST(event_params) AS ep
+    WHERE
+      event_name = 'page_view'
+      AND ep.key = 'page_title'
+      AND ${eventsBetween}
+    GROUP BY TRIM(SPLIT(REGEXP_REPLACE(ep.value.string_value, 'â€“', '-'), '-')[SAFE_OFFSET(0)])
+  ),
+  -- Total purchases per product
+  total_purchases AS (
+    SELECT
+      TRIM(i.item_name) AS product_name,
+      COUNT(DISTINCT (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'transaction_id')) AS total_purchases
+    FROM
+      \`fast-lattice-421210.analytics_274422295.events_*\`, UNNEST(items) AS i
+    WHERE
+      event_name = 'purchase'
+      AND ${eventsBetween}
+    GROUP BY TRIM(i.item_name)
+  ),
+  -- Default conversion rate calculation
+  default_conversion_rate AS (
+    SELECT
+      v.product_name,
+      v.total_views,
+      p.total_purchases,
+      ROUND(SAFE_DIVIDE(p.total_purchases, v.total_views) * 100, 2) AS default_conv_rate
+    FROM total_views AS v
+    JOIN total_purchases AS p
+      ON LOWER(v.product_name) = LOWER(p.product_name)
+  ),
+  -- Joining the CTEs to get the final table
+  final AS (
+    SELECT
+      a.product_name,
+      COALESCE(c._3D_Button_Clicks, 0) AS _3D_Button_Clicks,
+      COALESCE(b.AR_Button_Clicks, 0) AS AR_Button_Clicks,
+      COALESCE(d.purchases_with_service, 0) AS purchases_with_service,
+      COALESCE(dc.total_purchases, 0) AS total_purchases,
+      COALESCE(c._3D_Button_Clicks, 0) + COALESCE(b.AR_Button_Clicks, 0) AS total_button_clicks,
+      ROUND(SAFE_DIVIDE(COALESCE(d.purchases_with_service, 0), COALESCE(c._3D_Button_Clicks, 0) + COALESCE(b.AR_Button_Clicks, 0)) * 100, 2) AS product_conv_rate,
+      COALESCE(dc.total_views, 0) AS total_views,
+      COALESCE(dc.default_conv_rate, 0) AS default_conv_rate
+    FROM all_products AS a
+    LEFT JOIN ar_clicks AS b ON LOWER(a.product_name) = LOWER(b.page_title_product_name)
+    LEFT JOIN _3d_clicks AS c ON LOWER(a.product_name) = LOWER(c.page_title_product_name)
+    LEFT JOIN products_purchased_after_click_events AS d ON a.product_name = d.product_name
+    LEFT JOIN default_conversion_rate AS dc ON LOWER(a.product_name) = LOWER(dc.product_name)
+    WHERE COALESCE(c._3D_Button_Clicks, 0) + COALESCE(b.AR_Button_Clicks, 0) > 0
+    ORDER BY d.purchases_with_service DESC
+  )
+
+SELECT * FROM final
+WHERE total_button_clicks > 0 OR purchases_with_service > 0
+`,
 };
