8c20ab3c04ac995e1d3489b8f2191848e9c89ee4
Ilyes Khobizi <khobizi.ilyes@gmail.com>
2024-06-24 14:17:06 +0100

    Fixed AuthButton.

--- src/components/AuthButton.tsx
+++ src/components/AuthButton.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
-import { UserCircleIcon } from "@heroicons/react/24/outline";
+import { ArrowRightEndOnRectangleIcon } from "@heroicons/react/24/outline";
 
 import { createClient } from "@/utils/supabase/server";
 import { getUserWithMetadata } from "@/utils/supabase/getUser";
@@ -23,14 +23,14 @@ export default async function AuthButton() {
     <form action={signOut}>
       <button
         type="submit"
-        className="flex mx-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+        className="flex mx-3 text-sm  rounded-full md:mr-0 focus:ring-4 "
         id="user-menu-button"
         aria-expanded="false"
         data-dropdown-toggle="dropdown"
       >
-        <span className="sr-only">Open user menu</span>
+        <span className="sr-only">Sign out</span>
 
-        <UserCircleIcon className="h-8 w-8 rounded-full text-gray-400" />
+        <ArrowRightEndOnRectangleIcon className="h-8 w-8 rounded-full text-gray-400" />
       </button>
     </form>
   ) : (
