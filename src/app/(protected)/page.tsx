a1d83a307ff3cf7d97f9e11aa10d6657fd4bcdf4
Ilyes Khobizi <khobizi.ilyes@gmail.com>
2024-06-24 14:43:20 +0100

    CVRTable Card

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -98,15 +98,13 @@ export default function Index() {
       </div>
 
       <div className="col-span-2">
-        <Card>
-          <CVRTable
-            rows={clientQueryResult}
-            showColumns={{
-              total_purchases: false,
-              purchases_with_service: false,
-            }}
-          />
-        </Card>
+        <CVRTable
+          rows={clientQueryResult}
+          showColumns={{
+            total_purchases: false,
+            purchases_with_service: false,
+          }}
+        />
       </div>
     </div>
   );
--- src/components/CVRTable.tsx
+++ src/components/CVRTable.tsx
@@ -1,5 +1,6 @@
 import { executeClientQuery } from "@/utils/BigQuery/CVR";
 import {
+  Card,
   Table,
   TableBody,
   TableCell,
@@ -24,82 +25,94 @@ export default function CVRTable({ rows, showColumns }: CVRTableProps) {
   const isLoading = !rows;
 
   return (
-    <Table className="table-fixed">
-      <TableHead>
-        <TableRow>
-          <TableHeaderCell className="w-15">Product name</TableHeaderCell>
-          <TableHeaderCell className="text-right">AR Sessions</TableHeaderCell>
-          <TableHeaderCell className="text-right">3D Sessions</TableHeaderCell>
-          <TableHeaderCell className="text-right">CVR</TableHeaderCell>
-          <TableHeaderCell className="text-right">
-            CVR (CharpstAR)
-          </TableHeaderCell>
-
-          <TableHeaderCell className="text-right">Total Clicks</TableHeaderCell>
-
-          {showColumns.total_purchases && (
-            <TableHeaderCell className="text-right">
-              Total Purchases
-            </TableHeaderCell>
-          )}
-
-          {showColumns.purchases_with_service && (
-            <TableHeaderCell className="text-right">
-              Purchase with Service
-            </TableHeaderCell>
-          )}
-        </TableRow>
-      </TableHead>
-
-      <TableBody className="text-white">
-        {isLoading && (
+    <Card>
+      <Table className="table-fixed">
+        <TableHead>
           <TableRow>
-            <TableCell colSpan={8}>
-              <TableSkeleton />
-            </TableCell>
+            <TableHeaderCell className="w-15">Product name</TableHeaderCell>
+            <TableHeaderCell className="text-right">
+              AR Sessions
+            </TableHeaderCell>
+            <TableHeaderCell className="text-right">
+              3D Sessions
+            </TableHeaderCell>
+            <TableHeaderCell className="text-right">CVR</TableHeaderCell>
+            <TableHeaderCell className="text-right">
+              CVR (CharpstAR)
+            </TableHeaderCell>
+
+            <TableHeaderCell className="text-right">
+              Total Clicks
+            </TableHeaderCell>
+
+            {showColumns.total_purchases && (
+              <TableHeaderCell className="text-right">
+                Total Purchases
+              </TableHeaderCell>
+            )}
+
+            {showColumns.purchases_with_service && (
+              <TableHeaderCell className="text-right">
+                Purchase with Service
+              </TableHeaderCell>
+            )}
           </TableRow>
-        )}
+        </TableHead>
 
-        {rows && rows.length === 0 && (
-          <TableRow>
-            <TableCell colSpan={8} className="text-center">
-              No data available
-            </TableCell>
-          </TableRow>
-        )}
-
-        {rows &&
-          rows.map((row, i) => (
-            <TableRow key={i}>
-              <TableCell>
-                <div className="w-15 whitespace-normal">{row.product_name}</div>
+        <TableBody className="text-white">
+          {isLoading && (
+            <TableRow>
+              <TableCell colSpan={8}>
+                <TableSkeleton />
               </TableCell>
-              <TableCell className="text-right">
-                {row.AR_Button_Clicks}
-              </TableCell>
-              <TableCell className="text-right">
-                {row._3D_Button_Clicks}
-              </TableCell>
-              <TableCell className="text-right">{row.CVR.default}</TableCell>
-              <TableCell className="text-right">{row.CVR.charpstAR}</TableCell>
-              <TableCell className="text-right">
-                {row.total_button_clicks}
-              </TableCell>
-
-              {showColumns.total_purchases && (
-                <TableCell className="text-right">
-                  {row.total_purchases}
-                </TableCell>
-              )}
-
-              {showColumns.purchases_with_service && (
-                <TableCell className="text-right">
-                  {row.purchases_with_service}
-                </TableCell>
-              )}
             </TableRow>
-          ))}
-      </TableBody>
-    </Table>
+          )}
+
+          {rows && rows.length === 0 && (
+            <TableRow>
+              <TableCell colSpan={8} className="text-center">
+                No data available
+              </TableCell>
+            </TableRow>
+          )}
+
+          {rows &&
+            rows.map((row, i) => (
+              <TableRow key={i}>
+                <TableCell>
+                  <div className="w-15 whitespace-normal">
+                    {row.product_name}
+                  </div>
+                </TableCell>
+                <TableCell className="text-right">
+                  {row.AR_Button_Clicks}
+                </TableCell>
+                <TableCell className="text-right">
+                  {row._3D_Button_Clicks}
+                </TableCell>
+                <TableCell className="text-right">{row.CVR.default}</TableCell>
+                <TableCell className="text-right">
+                  {row.CVR.charpstAR}
+                </TableCell>
+                <TableCell className="text-right">
+                  {row.total_button_clicks}
+                </TableCell>
+
+                {showColumns.total_purchases && (
+                  <TableCell className="text-right">
+                    {row.total_purchases}
+                  </TableCell>
+                )}
+
+                {showColumns.purchases_with_service && (
+                  <TableCell className="text-right">
+                    {row.purchases_with_service}
+                  </TableCell>
+                )}
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+    </Card>
   );
 }
