81af8d5de867bfe91f2e0bd934937179a3884910
Ilyes Khobizi <khobizi.ilyes@gmail.com>
2024-06-27 01:10:20 +0100

    chore: Refactor AppSide component and its dependencies and adding active link.

---
+++ src/components/AppLayout/AppSide/components.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { classNames } from "@/utils/uiUtils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export function ListItem({
+  icon,
+  title,
+  href,
+}: {
+  icon: any;
+  title: string;
+  href: string;
+}) {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+
+  return (
+    <li>
+      <Link
+        href={href}
+        className={classNames(
+          "flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group",
+
+          isActive && "bg-gray-100 dark:bg-gray-700",
+        )}
+      >
+        {icon}
+        <span className="ml-3">{title}</span>
+      </Link>
+    </li>
+  );
+}
+
+export function MiniAction({ children }: React.PropsWithChildren) {
+  return (
+    <Link
+      href="/"
+      type="button"
+      data-dropdown-toggle="language-dropdown"
+      className="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer dark:hover:text-white dark:text-gray-400 hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600"
+    >
+      {children}
+    </Link>
+  );
+}
--- src/components/AppLayout/AppSide/index.tsx
+++ src/components/AppLayout/AppSide/index.tsx
@@ -1,39 +1,4 @@
-import Link from "next/link";
-
-function ListItem({
-  icon,
-  title,
-  href,
-}: {
-  icon: any;
-  title: string;
-  href: string;
-}) {
-  return (
-    <li>
-      <Link
-        href={href}
-        className="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-      >
-        {icon}
-        <span className="ml-3">{title}</span>
-      </Link>
-    </li>
-  );
-}
-
-function MiniAction({ children }: React.PropsWithChildren) {
-  return (
-    <Link
-      href="/"
-      type="button"
-      data-dropdown-toggle="language-dropdown"
-      className="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer dark:hover:text-white dark:text-gray-400 hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600"
-    >
-      {children}
-    </Link>
-  );
-}
+import { ListItem } from "./components";
 
 export default function AppSide() {
   return (
@@ -101,7 +66,7 @@ export default function AppSide() {
               </svg>
             }
             title="Documentation"
-            href="/"
+            href="/docs"
           />
         </ul>
       </div>
