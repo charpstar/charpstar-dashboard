9be3a8345515de81b4a0f5667655868d31771a29
darklift <darklift@gmail.com>
2024-07-04 12:48:54 +0200

    BigQuery Production Connection

--- src/utils/BigQuery/getEventsCount.ts
+++ src/utils/BigQuery/getEventsCount.ts
@@ -1,6 +1,25 @@
 "use server";
 
-import { getBigQueryClient } from "./client";
+import { BigQuery } from "@google-cloud/bigquery";
+import fs from "fs";
+import path from "path";
+
+// Function to initialize BigQuery client
+// Function to initialize BigQuery client
+function getBigQueryClient(projectId: string) {
+  // If GOOGLE_APPLICATION_CREDENTIALS is set, write the content to a file
+  if (process.env.GOOGLE_APPLICATION_CREDENTIALS_CONTENT) {
+// ... existing code ...
+    const credentialsPath = path.join(__dirname, "google-credentials.json");
+    fs.writeFileSync(credentialsPath, process.env.GOOGLE_APPLICATION_CREDENTIALS_CONTENT);
+    process.env.GOOGLE_APPLICATION_CREDENTIALS = credentialsPath;
+  }
+
+  return new BigQuery({
+    projectId,
+    keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS
+  });
+}
 
 export async function getEventsCount({
   projectId,
@@ -12,236 +31,236 @@ export async function getEventsCount({
   datasetId: string;
   startTableName: string;
   endTableName: string;
-}): Promise<Record<string, number>> {
-  const { value: bigqueryClient } = getBigQueryClient({ projectId });
+}) {
+  if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+  }
+
+  const bigqueryClient = getBigQueryClient(projectId);
 
   const query = `
-  WITH
-  total_views AS (
-    SELECT
-      COUNT(DISTINCT user_pseudo_id) AS total_views
-    FROM
-      \`${projectId}.${datasetId}.events_*\`,
-      UNNEST(event_params) AS ep
-    WHERE
-      event_name = 'page_view'
-      AND ep.key = 'page_title'
-      AND _TABLE_SUFFIX BETWEEN '${startTableName}' AND '${endTableName}'
-  ),
-  total_purchases AS (
-    SELECT
-      COUNT(DISTINCT (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'transaction_id')) AS total_purchases
-    FROM
+    WITH
+    total_views AS (
+      SELECT
+        COUNT(DISTINCT user_pseudo_id) AS total_views
+      FROM
+        \`${projectId}.${datasetId}.events_*\`,
+        UNNEST(event_params) AS ep
+      WHERE
+        event_name = 'page_view'
+        AND ep.key = 'page_title'
+        AND _TABLE_SUFFIX BETWEEN '${startTableName}' AND '${endTableName}'
+    ),
+    total_purchases AS (
+      SELECT
+        COUNT(DISTINCT (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'transaction_id')) AS total_purchases
+      FROM
+        \`${projectId}.${datasetId}.events_*\`
+      WHERE
+        event_name = 'purchase'
+        AND _TABLE_SUFFIX BETWEEN '${startTableName}' AND '${endTableName}'
+    ),
+    ar_clicks AS (
+      SELECT
+        user_pseudo_id,
+        event_timestamp
+      FROM
+        \`${projectId}.${datasetId}.events_*\`
+      WHERE
+        event_name IN ('charpstAR_AR_Button_Click', 'charpstAR_3D_Button_Click')
+        AND _TABLE_SUFFIX BETWEEN '${startTableName}' AND '${endTableName}'
+    ),
+    purchases AS (
+      SELECT
+        user_pseudo_id,
+        event_timestamp,
+        (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'transaction_id') AS transaction_id
+      FROM
+        \`${projectId}.${datasetId}.events_*\`
+      WHERE
+        event_name = 'purchase'
+        AND _TABLE_SUFFIX BETWEEN '${startTableName}' AND '${endTableName}'
+    ),
+    total_views_with_ar AS (
+      SELECT
+        COUNT(DISTINCT ar.user_pseudo_id) AS total_views_with_ar
+      FROM
+        ar_clicks AS ar
+    ),
+    total_purchases_with_ar AS (
+      SELECT
+        COUNT(DISTINCT p.transaction_id) AS total_purchases_with_ar
+      FROM
+        ar_clicks AS ar
+        JOIN purchases AS p
+        ON ar.user_pseudo_id = p.user_pseudo_id
+        AND p.event_timestamp > ar.event_timestamp
+    ),
+    conversion_rates AS (
+      SELECT
+        ROUND(SAFE_DIVIDE(tp.total_purchases, tv.total_views) * 100, 2) AS overall_avg_conversion_rate,
+        ROUND(SAFE_DIVIDE(tp_ar.total_purchases_with_ar, tv_ar.total_views_with_ar) * 100, 2) AS overall_avg_conversion_rate_with_ar
+      FROM
+        total_views AS tv,
+        total_purchases AS tp,
+        total_views_with_ar AS tv_ar,
+        total_purchases_with_ar AS tp_ar
+    ),
+    event_counts AS (
+      SELECT
+        'charpstAR_Load' AS event_name,
+        COUNT(*) AS total_events
+      FROM
+        \`${projectId}.${datasetId}.events_*\`
+      WHERE
+        event_name = 'charpstAR_Load'
+        AND _TABLE_SUFFIX BETWEEN '${startTableName}' AND '${endTableName}'
+      UNION ALL
+      SELECT
+        'charpstAR_AR_Button_Click' AS event_name,
+        COUNT(*) AS total_events
+      FROM
+        \`${projectId}.${datasetId}.events_*\`
+      WHERE
+        event_name = 'charpstAR_AR_Button_Click'
+        AND _TABLE_SUFFIX BETWEEN '${startTableName}' AND '${endTableName}'
+      UNION ALL
+      SELECT
+        'charpstAR_3D_Button_Click' AS event_name,
+        COUNT(*) AS total_events
+      FROM
+        \`${projectId}.${datasetId}.events_*\`
+      WHERE
+        event_name = 'charpstAR_3D_Button_Click'
+        AND _TABLE_SUFFIX BETWEEN '${startTableName}' AND '${endTableName}'
+    ),
+    ar_percentage AS (
+      SELECT
+        ROUND(
+          SAFE_DIVIDE(
+            (SELECT SUM(total_events) FROM event_counts WHERE event_name IN ('charpstAR_AR_Button_Click', 'charpstAR_3D_Button_Click')),
+            (SELECT total_events FROM event_counts WHERE event_name = 'charpstAR_Load')
+          ) * 100,
+          2
+        ) AS percentage_ar_users
+    ),
+    avg_engagement_time AS (
+      SELECT
+        AVG((SELECT value.int_value 
+             FROM UNNEST(event_params) ep 
+             WHERE ep.key = 'engagement_time_msec') / 1000.0) AS avg_session_duration_seconds
+      FROM
       \`${projectId}.${datasetId}.events_*\`
-    WHERE
-      event_name = 'purchase'
-      AND _TABLE_SUFFIX BETWEEN '${startTableName}' AND '${endTableName}'
-  ),
-  ar_clicks AS (
+      WHERE
+        event_name IN ('page_view', 'user_engagement')
+        AND _TABLE_SUFFIX BETWEEN '${startTableName}' AND '${endTableName}'
+    ),
+    ar_events AS (
+      SELECT
+        user_pseudo_id,
+        event_timestamp
+      FROM
+        \`${projectId}.${datasetId}.events_*\`
+      WHERE
+        event_name IN ('charpstAR_AR_Button_Click', 'charpstAR_3D_Button_Click')
+        AND _TABLE_SUFFIX BETWEEN '${startTableName}' AND '${endTableName}'
+    ),
+    next_events AS (
+      SELECT
+        ar.user_pseudo_id,
+        ar.event_timestamp AS ar_event_timestamp,
+        MIN(e.event_timestamp) / 1000 AS next_event_timestamp
+      FROM
+        ar_events AS ar
+      JOIN
+      \`${projectId}.${datasetId}.events_*\` AS e
+      ON
+        ar.user_pseudo_id = e.user_pseudo_id
+        AND e.event_timestamp > ar.event_timestamp
+      GROUP BY
+        ar.user_pseudo_id, ar.event_timestamp
+    ),
+    ar_durations AS (
+      SELECT
+        SAFE_DIVIDE(ne.next_event_timestamp - ar.event_timestamp / 1000, 1000) AS interaction_duration_seconds
+      FROM
+        ar_events AS ar
+      LEFT JOIN
+        next_events AS ne
+      ON
+        ar.user_pseudo_id = ne.user_pseudo_id
+        AND ar.event_timestamp = ne.ar_event_timestamp
+      WHERE
+        ne.next_event_timestamp IS NOT NULL
+        AND SAFE_DIVIDE(ne.next_event_timestamp - ar.event_timestamp / 1000, 1000) BETWEEN 0 AND 3600
+    ),
+    avg_ar_duration AS (
+      SELECT
+        AVG(interaction_duration_seconds) AS avg_ar_session_duration_seconds
+      FROM
+        ar_durations
+    ),
+    combined_durations AS (
+      SELECT
+        (SELECT avg_ar_session_duration_seconds FROM avg_ar_duration) + 
+        (SELECT avg_session_duration_seconds FROM avg_engagement_time) AS total_avg_session_duration
+    )
+      
     SELECT
-      user_pseudo_id,
-      event_timestamp
+      'overall_conv_rate' AS event_name,
+      overall_avg_conversion_rate AS count
     FROM
-      \`${projectId}.${datasetId}.events_*\`
-    WHERE
-      event_name IN ('charpstAR_AR_Button_Click', 'charpstAR_3D_Button_Click')
-      AND _TABLE_SUFFIX BETWEEN '${startTableName}' AND '${endTableName}'
-  ),
-  purchases AS (
-    SELECT
-      user_pseudo_id,
-      event_timestamp,
-      (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'transaction_id') AS transaction_id
-    FROM
-      \`${projectId}.${datasetId}.events_*\`
-    WHERE
-      event_name = 'purchase'
-      AND _TABLE_SUFFIX BETWEEN '${startTableName}' AND '${endTableName}'
-  ),
-  total_views_with_ar AS (
-    SELECT
-      COUNT(DISTINCT ar.user_pseudo_id) AS total_views_with_ar
-    FROM
-      ar_clicks AS ar
-  ),
-  total_purchases_with_ar AS (
-    SELECT
-      COUNT(DISTINCT p.transaction_id) AS total_purchases_with_ar
-    FROM
-      ar_clicks AS ar
-      JOIN purchases AS p
-      ON ar.user_pseudo_id = p.user_pseudo_id
-      AND p.event_timestamp > ar.event_timestamp
-  ),
-  conversion_rates AS (
-    SELECT
-      ROUND(SAFE_DIVIDE(tp.total_purchases, tv.total_views) * 100, 2) AS overall_avg_conversion_rate,
-      ROUND(SAFE_DIVIDE(tp_ar.total_purchases_with_ar, tv_ar.total_views_with_ar) * 100, 2) AS overall_avg_conversion_rate_with_ar
-    FROM
-      total_views AS tv,
-      total_purchases AS tp,
-      total_views_with_ar AS tv_ar,
-      total_purchases_with_ar AS tp_ar
-  ),
-  event_counts AS (
-    SELECT
-      'charpstAR_Load' AS event_name,
-      COUNT(*) AS total_events
-    FROM
-      \`${projectId}.${datasetId}.events_*\`
-    WHERE
-      event_name = 'charpstAR_Load'
-      AND _TABLE_SUFFIX BETWEEN '${startTableName}' AND '${endTableName}'
+      conversion_rates
+      
     UNION ALL
+      
     SELECT
-      'charpstAR_AR_Button_Click' AS event_name,
-      COUNT(*) AS total_events
+      'overall_conv_rate_CharpstAR' AS event_name,
+      overall_avg_conversion_rate_with_ar AS count
     FROM
-      \`${projectId}.${datasetId}.events_*\`
-    WHERE
-      event_name = 'charpstAR_AR_Button_Click'
-      AND _TABLE_SUFFIX BETWEEN '${startTableName}' AND '${endTableName}'
+      conversion_rates
+      
     UNION ALL
+      
     SELECT
-      'charpstAR_3D_Button_Click' AS event_name,
-      COUNT(*) AS total_events
+      event_name AS event_name,
+      total_events AS count
     FROM
-      \`${projectId}.${datasetId}.events_*\`
-    WHERE
-      event_name = 'charpstAR_3D_Button_Click'
-      AND _TABLE_SUFFIX BETWEEN '${startTableName}' AND '${endTableName}'
-  ),
-  ar_percentage AS (
+      event_counts
+      
+    UNION ALL
+      
     SELECT
-      ROUND(
-        SAFE_DIVIDE(
-          (SELECT SUM(total_events) FROM event_counts WHERE event_name IN ('charpstAR_AR_Button_Click', 'charpstAR_3D_Button_Click')),
-          (SELECT total_events FROM event_counts WHERE event_name = 'charpstAR_Load')
-        ) * 100,
-        2
-      ) AS percentage_ar_users
-  ),
-  avg_engagement_time AS (
-    SELECT
-      AVG((SELECT value.int_value 
-           FROM UNNEST(event_params) ep 
-           WHERE ep.key = 'engagement_time_msec') / 1000.0) AS avg_session_duration_seconds
+      'percentage_charpstAR' AS event_name,
+      percentage_ar_users AS count 
     FROM
-    \`${projectId}.${datasetId}.events_*\`
-    WHERE
-      event_name IN ('page_view', 'user_engagement')
-      AND _TABLE_SUFFIX BETWEEN '${startTableName}' AND '${endTableName}'
-  ),
- ar_events AS (
-  SELECT
-    user_pseudo_id,
-    event_timestamp,
-  FROM
-  \`${projectId}.${datasetId}.events_*\`
-  WHERE
-    event_name IN ('charpstAR_AR_Button_Click', 'charpstAR_3D_Button_Click')
-    AND _TABLE_SUFFIX BETWEEN '${startTableName}' AND '${endTableName}'
-),
-next_events AS (
-  SELECT
-    ar.user_pseudo_id,
-    ar.event_timestamp AS ar_event_timestamp,
-    MIN(e.event_timestamp) / 1000 AS next_event_timestamp,
-  FROM
-    ar_events AS ar
-  JOIN
-  \`${projectId}.${datasetId}.events_*\` AS e
-  ON
-    ar.user_pseudo_id = e.user_pseudo_id
-    AND e.event_timestamp > ar.event_timestamp
-  GROUP BY
-    ar.user_pseudo_id, ar.event_timestamp
-),
-ar_durations AS (
-  SELECT
-    SAFE_DIVIDE(ne.next_event_timestamp - ar.event_timestamp / 1000, 1000) AS interaction_duration_seconds
-  FROM
-    ar_events AS ar
-  LEFT JOIN
-    next_events AS ne
-  ON
-    ar.user_pseudo_id = ne.user_pseudo_id
-    AND ar.event_timestamp = ne.ar_event_timestamp
-  WHERE
-    ne.next_event_timestamp IS NOT NULL
-    AND SAFE_DIVIDE(ne.next_event_timestamp - ar.event_timestamp / 1000, 1000) BETWEEN 0 AND 3600
-),
-avg_ar_duration AS (
-  SELECT
-    AVG(interaction_duration_seconds) AS avg_ar_session_duration_seconds
-  FROM
-    ar_durations
-),
-
-combined_durations AS (
-  SELECT
-    (SELECT avg_ar_session_duration_seconds FROM avg_ar_duration) + 
-    (SELECT avg_session_duration_seconds FROM avg_engagement_time) AS total_avg_session_duration
-)
-  
-  SELECT
-    'overall_conv_rate' AS event_name,
-    overall_avg_conversion_rate AS count
-  FROM
-    conversion_rates
-  
-  UNION ALL
-  
-  SELECT
-    'overall_conv_rate_CharpstAR' AS event_name,
-    overall_avg_conversion_rate_with_ar AS count
-  FROM
-    conversion_rates
-  
-  UNION ALL
-  
-  SELECT
-    event_name AS event_name,
-    total_events AS count
-  FROM
-    event_counts
-  
-  UNION ALL
-  
-  SELECT
-    'percentage_charpstAR' AS event_name,
-    percentage_ar_users AS count 
-  FROM
-    ar_percentage
-  
-  UNION ALL
-
-  SELECT
-  'session_time_charpstAR' AS event_name,
-  ROUND(avg_ar_session_duration_seconds, 2) AS count
-FROM
-  avg_ar_duration
-
-UNION ALL
-
-SELECT
-  'session_time_default' AS event_name,
-  ROUND(avg_session_duration_seconds, 2) AS count
-FROM
-  avg_engagement_time
-
-UNION ALL
-
-SELECT
-  'combined_session_time' AS event_name,
-  ROUND(total_avg_session_duration, 2) AS count
-FROM
-  combined_durations;
-
-
+      ar_percentage
+      
+    UNION ALL
+    
+    SELECT
+    'session_time_charpstAR' AS event_name,
+    ROUND(avg_ar_session_duration_seconds, 2) AS count
+    FROM
+    avg_ar_duration
+    
+    UNION ALL
+    
+    SELECT
+    'session_time_default' AS event_name,
+    ROUND(avg_session_duration_seconds, 2) AS count
+    FROM
+    avg_engagement_time
+    
+    UNION ALL
+    
+    SELECT
+    'combined_session_time' AS event_name,
+    ROUND(total_avg_session_duration, 2) AS count
+    FROM
+    combined_durations;
   `;
   
-
   const options = {
     query: query,
     projectId,
