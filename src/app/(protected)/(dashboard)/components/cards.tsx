1ffa4c62e4c811c4083bbb5d8bd5a97e58f83b20
Ilyes Khobizi <khobizi.ilyes@gmail.com>
2024-07-11 21:47:52 +0100

    Added proper cards.

--- src/app/(protected)/(dashboard)/components/cards.tsx
+++ src/app/(protected)/(dashboard)/components/cards.tsx
@@ -1,105 +1,94 @@
+"use client";
+
+import Skeleton from "@/components/Skeleton";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useEventsCount } from "@/queries/useEventsCount";
+
+function DashboardCard({
+  title,
+  content,
+  explanation,
+  icon,
+}: {
+  title: string;
+  content: string;
+  explanation: string;
+  icon: React.ReactNode;
+}) {
+  return (
+    <Card>
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-sm font-medium">{title}</CardTitle>
+        {icon}
+      </CardHeader>
+
+      <CardContent>
+        <div className="text-2xl font-bold">{content}</div>
+        <p className="text-xs text-muted-foreground">{explanation}</p>
+      </CardContent>
+    </Card>
+  );
+}
 
 export default function Cards() {
-  return (
-    <>
-      <Card>
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
+  const startTableName = "20240101";
+  const endTableName = "20240431";
 
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="h-4 w-4 text-muted-foreground"
-          >
-            <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-          </svg>
-        </CardHeader>
+  const { eventsCount, isEventsCountLoading } = useEventsCount({
+    startTableName,
+    endTableName,
+  });
 
-        <CardContent>
-          <div className="text-2xl font-bold">$45,231.89</div>
-          <p className="text-xs text-muted-foreground">
-            +20.1% from last month
-          </p>
-        </CardContent>
-      </Card>
+  if (isEventsCountLoading)
+    return (
+      <>
+        {Array(Object.keys(eventsCount).length)
+          .fill(undefined)
+          .map((_, i) => (
+            <Skeleton key={i} />
+          ))}
+      </>
+    );
 
-      <Card>
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">Subscriptions</CardTitle>
+  return Object.entries(eventsCount).map(([event_name, data]) => {
+    const isPercentageCard = [
+      "percentage_charpstAR",
+      "overall_conv_rate",
+      "overall_conv_rate_CharpstAR",
+    ].includes(event_name);
+    const formattedCount = isPercentageCard ? (
+      <>
+        <span>{data.count}</span>
+        <span style={{ fontSize: "0.8em" }}>%</span>
+      </>
+    ) : [
+        "charpstAR_Load",
+        "charpstAR_AR_Button_Click",
+        "charpstAR_3D_Button_Click",
+      ].includes(event_name) ? (
+      (data.count ?? 0).toLocaleString()
+    ) : ["combined_session_time", "session_time_default"].includes(
+        event_name,
+      ) ? (
+      <>
+        <span>{data.count ?? 0}</span>
+        <span style={{ fontSize: "0.5em", color: "#eee", fontWeight: "300" }}>
+          {" "}
+          seconds
+        </span>
+      </>
+    ) : (
+      data.count ?? 0
+    );
 
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="h-4 w-4 text-muted-foreground"
-          >
-            <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-            <circle cx="9" cy="7" r="4" />
-            <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-          </svg>
-        </CardHeader>
-
-        <CardContent>
-          <div className="text-2xl font-bold">+2350</div>
-          <p className="text-xs text-muted-foreground">
-            +180.1% from last month
-          </p>
-        </CardContent>
-      </Card>
-
-      <Card>
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">Sales</CardTitle>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="h-4 w-4 text-muted-foreground"
-          >
-            <rect width="20" height="14" x="2" y="5" rx="2" />
-            <path d="M2 10h20" />
-          </svg>
-        </CardHeader>
-        <CardContent>
-          <div className="text-2xl font-bold">+12,234</div>
-          <p className="text-xs text-muted-foreground">+19% from last month</p>
-        </CardContent>
-      </Card>
-      <Card>
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">Active Now</CardTitle>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="h-4 w-4 text-muted-foreground"
-          >
-            <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-          </svg>
-        </CardHeader>
-        <CardContent>
-          <div className="text-2xl font-bold">+573</div>
-          <p className="text-xs text-muted-foreground">+201 since last hour</p>
-        </CardContent>
-      </Card>
-    </>
-  );
+    return (
+      <DashboardCard
+        key={event_name}
+        {...data}
+        content={String(data.count ?? 0)}
+        explanation={String(formattedCount)}
+        icon={<div>Icon</div>}
+      />
+    );
+  });
 }
