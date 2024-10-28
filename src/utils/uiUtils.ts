ff6a55a3735340d2b4e97f4e046ac33289d939cf
darklift <darklift@gmail.com>
2024-10-28 13:14:43 +0100

    Date update 15 days

--- src/utils/uiUtils.ts
+++ src/utils/uiUtils.ts
@@ -12,10 +12,10 @@ export function compToBq(date: string) {
 export function buildDateRange(startDate?: dayjs.Dayjs, endDate?: dayjs.Dayjs) {
   return {
     startDate: dayjsToComp(
-      startDate ?? customShortcutsDayJs.last60days!.period.start,
+      startDate ?? customShortcutsDayJs.last15days!.period.start,
     ),
     endDate: dayjsToComp(
-      endDate ?? customShortcutsDayJs.last60days!.period.end,
+      endDate ?? customShortcutsDayJs.last15days!.period.end,
     ),
   };
 }
